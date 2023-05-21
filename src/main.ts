866d3cad5afcd5b22ae610c0952e20d8da1a3c69
vwkd <33468089+vwkd@users.noreply.github.com>
2023-05-21 23:38:31 +0200

    main: add todo (old changes, unsure if valid)

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { rehypeSplitWhitespace, rehypeUnwrap } from "../deps.ts";
  * Unwraps leading and trailing space from inline nodes
  * into new text node in closest ancestor where branch is not first or last child
  */
+// todo: doesn't work because also moves up valid whitespace notes, e.g. `**foo** **bar**` becomes `**foo ****bar**`
 const rehypeUnwrapSpaces = [rehypeSplitWhitespace, [rehypeUnwrap, {
   childTest: (node) => node.type == "text" && node.value.match(/^\s+$/),
   parentTest: (node) => ["em", "a", "strong"].includes(node?.tagName),
